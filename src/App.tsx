6a02099996406d2e25d8613c70adfa1747af9075
andrei-mushovets <andreimyshoves89@gmail.com>
2026-05-26 17:12:47 +0200

    add wouter and shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
-function App() {
-    return <div>Bootcamp Delivery</div>
-}
+import { Route, Switch } from 'wouter'
+import HomePage from '@/pages/HomePage/HomePage'
+import ProfilePage from '@/pages/ProfilePage/ProfilePage'
 
+function App() {
+    return (
+        <Switch>
+            <Route path="/" component={HomePage} />
+            <Route path="/profile" component={ProfilePage} />
+        </Switch>
+    )
+}
 export default App
