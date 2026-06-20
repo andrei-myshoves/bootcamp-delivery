ce50c6eb93f44d6050fcbc9856f57574c509f481
andrei-mushovets <andreimyshoves89@gmail.com>
2026-06-20 08:51:17 +0200

    add prettir ogr select ui file

--- src/components/ui/ui-select.tsx
+++ src/components/ui/ui-select.tsx
@@ -1,42 +1,30 @@
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from './select'
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './select'
 
 interface Option {
-  value: string
-  label: string
+    value: string
+    label: string
 }
 
 interface UiSelectProps {
-  placeholder?: string
-  options: Option[]
-	defaultValue?: string
+    placeholder?: string
+    options: Option[]
+    defaultValue?: string
 }
 
-export function UiSelect({
-  placeholder,
-  options,
-}: UiSelectProps) {
-  return (
-    <Select>
-      <SelectTrigger>
-        <SelectValue placeholder={placeholder} />
-      </SelectTrigger>
+export function UiSelect({ placeholder, options }: UiSelectProps) {
+    return (
+        <Select>
+            <SelectTrigger>
+                <SelectValue placeholder={placeholder} />
+            </SelectTrigger>
 
-      <SelectContent>
-        {options.map((option) => (
-          <SelectItem
-            key={option.value}
-            value={option.value}
-          >
-            {option.label}
-          </SelectItem>
-        ))}
-      </SelectContent>
-    </Select>
-  )
+            <SelectContent>
+                {options.map(option => (
+                    <SelectItem key={option.value} value={option.value}>
+                        {option.label}
+                    </SelectItem>
+                ))}
+            </SelectContent>
+        </Select>
+    )
 }
