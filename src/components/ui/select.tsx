a6b155d68178d0f6de0e6bd316a76ea0df082aee
andrei-mushovets <andreimyshoves89@gmail.com>
2026-06-28 08:26:59 +0200

    rename file ui-select

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -1,4 +1,4 @@
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './select'
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './select-primitive'
 
 interface Option {
     value: string
