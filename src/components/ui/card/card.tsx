606ca83364d65ad218f535a4d5365deb5b5bb3ce
andrei-mushovets <andreimyshoves89@gmail.com>
2026-07-01 08:15:20 +0200

    delete unneseary interface in file card.tsx

--- src/components/ui/card/card.tsx
+++ src/components/ui/card/card.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 
 import { cn } from '@/shared/lib/utils'
 
-interface CardProps extends React.HTMLAttributes<HTMLDivElement> {}
+type CardProps = React.HTMLAttributes<HTMLDivElement>
 
 function Card({ className, children, ...props }: CardProps) {
     return (
