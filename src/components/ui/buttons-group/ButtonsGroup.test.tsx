81ebb72fba02fab92c5114c2db698b42745debe8
andrei-mushovets <andreimyshoves89@gmail.com>
2026-07-11 10:06:42 +0200

    add true import for buttonsgroup test

--- src/components/ui/buttons-group/ButtonsGroup.test.tsx
+++ src/components/ui/buttons-group/ButtonsGroup.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { describe, expect, it, vi } from 'vitest'
 
-import { ButtonsGroup } from './buttons-group'
+import { ButtonsGroup } from './ButtonsGroup'
 
 describe('ButtonsGroup', () => {
     const options = [
