eecf643ba218beec4e3575d72657f62d80fc1e5a
andrei-mushovets <andreimyshoves89@gmail.com>
2026-07-29 15:20:35 +0200

    add pretier

--- src/components/layout/header/Logo.tsx
+++ src/components/layout/header/Logo.tsx
@@ -3,7 +3,7 @@ import { Link } from 'wouter'
 
 export function Logo() {
     return (
-        <Link href='/'>
+        <Link href="/">
             <img src={LogoDelivery} alt="Delivery" className="h-6 w-auto" />
         </Link>
     )
