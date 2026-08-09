894fb8933bfe91be8508011c6b8c2caeec13b704
andrei-mushovets <andreimyshoves89@gmail.com>
2026-08-09 08:56:14 +0200

    tune import in sheet sorit=es

--- src/components/ui/sheet/sheet.stories.tsx
+++ src/components/ui/sheet/sheet.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/react'
+import type { Meta, StoryObj } from '@storybook/react-vite'
 
 import { Sheet, SheetTrigger, SheetContent, SheetHeader, SheetTitle, SheetDescription } from './sheet'
 
